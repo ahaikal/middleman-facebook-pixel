60e9ad894673d73f4b1c8a3b462b3816806fbe80
Kairat Jenishev <kairat.jenishev@gmail.com>
2016-07-27 15:28:39 +0600

    Raise error if no property ID given

--- lib/middleman-facebook-pixel/extension.rb
+++ lib/middleman-facebook-pixel/extension.rb
@@ -1,8 +1,16 @@
+require 'active_support/core_ext/object/blank'
 require 'middleman-facebook-pixel/helpers'
 
 module Middleman
   class FacebookPixelExtension < Extension
     option :id, nil, 'Facebook Pixel ID'
+
+    def after_configuration
+      if options.id.blank?
+        $stderr.puts 'Facebook Pixel: Please specify a property ID'
+        raise ArgumentError, 'No property ID given'
+      end
+    end
 
     helpers do
       include ::Middleman::FacebookPixel::Helpers
--- lib/middleman-facebook-pixel/version.rb
+++ lib/middleman-facebook-pixel/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   module FacebookPixel
-    VERSION = '0.1.0'
+    VERSION = '0.1.1'
   end
 end
